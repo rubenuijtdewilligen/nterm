05d07e07eee635a643add03ee809a0a6333c5468
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2024-05-16 21:13:08 +0200

    Add w3counter analytics

---
+++ src/lib/Analytics.svelte
@@ -0,0 +1,3 @@
+<svelte:head>
+  <script src="https://www.w3counter.com/tracker.js?id=152392"></script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
   import '../app.css';
+  import Analytics from '$lib/Analytics.svelte';
 </script>
+
+<Analytics />
 
 <div class="flex h-screen items-center justify-center">
   <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
